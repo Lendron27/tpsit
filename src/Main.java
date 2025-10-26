6d54f785b9448e1f4f57e90792171f10a6cfdd1a
Lorenzo G <loree.giul@gmail.com>
2025-10-26 18:58:01 +0100

    Secondo commit

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,77 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Scanner tastiera= new Scanner(System.in);
         System.out.println("Quanto è lungo il percorso?");
         int count= tastiera.nextInt(); //.nextLine() per leggere la riga
-        //singolo thread
-        Gara thr1 = new Gara(count);
-        Gara thr2 = new Gara(count);
-        Gara thr3 = new Gara(count);
-        Gara thr4 = new Gara(count);
-        Gara thr5 = new Gara(count);
-        thr1.start();
-        thr2.start();
-        thr3.start();
-        thr4.start();
-        thr5.start();
+        // crea e avvia 5  Cavallo
+        Gara[] cavalli = new Gara[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Inserisci nome cavallo: ");
+            String nome = tastiera.next();
+            cavalli[i] = new Gara(nome, count);
+        }
+
+        System.out.println("Cavalli in partenza:");
+        for (int i = 0; i < 5; i++) {
+            System.out.println((i + 1) + ") " + cavalli[i].getNome());
+        }
+
+        for (int i = 0; i < 5; i++) {
+            cavalli[i].start();
+        }
+
+        for (int i = 0; i < 5; i++) {
+            cavalli[i].join();
+        }
+
+        ArrayList<String> classifica = Gara.getTabella();
+        System.out.println("Classifica:");
+        for (int i = 0; i < classifica.size(); i++) {
+            System.out.println((i + 1) + ") " + classifica.get(i));
+        }
     }
 }
 
 class Gara extends Thread {
     //variabile privata
+    private int percorso = 0;
     private final int lungh;
+    private final String nome;
+    private static ArrayList<String> tabella = new ArrayList<>();
     //costruttore
-    public Gara(int num){
+    public Gara(String nome, int num){
         super();
+        this.nome = nome;
         lungh=num;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
     @Override
     public void run() {
-        Scanner tastiera= new Scanner(System.in);
-        System.out.println("Come si chiama il cavallo?");
-        setName(tastiera.nextLine());
-        for (int i = 0; i < lungh; i+=5) {
-            System.out.println((i + 1) + " metri fatti da " + Thread.currentThread().getName());
+        while (percorso < lungh) {
+            percorso += 5;
+            System.out.println(percorso + "m percorsi da " + nome);
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+
+        synchronized (tabella) {
+            tabella.add(nome);
+        }
+    }
+
+    public static ArrayList<String> getTabella() {
+        return tabella;
     }
 }
